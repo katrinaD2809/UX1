e58bda64f140cb67f6abd8b9066ae4b8af0044bb
katrinaD2809 <88752233+katrinaD2809@users.noreply.github.com>
2022-03-08 13:31:33 +1000

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,27 @@
+nav bar includes search field
+
+homepage
+
+2 journeys 
+1. buy subscription for myself
+2. buy for someone else
+
+1. buy subscription for myself
+- questionaire boxes and radio buttons 1. age 2. gender 3. experienced gardener or beginner 4. indoor/outdoor. 5. if indoor what room? 6. if outdoor full sun or shade? 7. high maintenance/low maintenance
+- length of subscription as radio buttons 3 months 6 months 12 months
+- delivery details form - name address postcode city state.
+- next page is payment page  
+
+2. buy for someone else
+- questionaire boxes and radio buttons 1. age 2. gender 3. experienced gardener or beginner 4. indoor/outdoor.
+- length of subscription as radio buttons 3 months 6 months 12 months
+- delivery details form - name address postcode city state.
+- next page is payment page  
+
+pricing page - table 
+
+payment page - enter account and credit card details
+
+log in to community - get support, help on plant - what platform to link to ??
+log in to account - track order, pause subscription. takes to secure page with all details.
+
---
+++ script/script.js
@@ -0,0 +1,81 @@
+let page1 = document.getElementById("page1")
+let page2 = document.getElementById("page2")
+let page3 = document.getElementById("page3")
+let page4 = document.getElementById("page4")
+let page5 = document.getElementById("page5")
+let page6 = document.getElementById("page6")
+let page7 = document.getElementById("page7")
+
+function showPage1() {
+    page1.style.display = "block"
+    page2.style.display = "none"
+    page3.style.display = "none"
+    page4.style.display = "none"
+    page5.style.display = "none"
+    page6.style.display = "none"
+    page7.style.display = "none"
+}
+
+function showPage2() {
+    page1.style.display = "none"
+    page2.style.display = "block"
+    page3.style.display = "none"
+    page4.style.display = "none" 
+    page5.style.display = "none"
+    page6.style.display = "none"
+    page7.style.display = "none"
+}
+
+function showPage3() {
+    page1.style.display = "none"
+    page2.style.display = "none"
+    page3.style.display = "block"
+    page4.style.display = "none"
+    page5.style.display = "none"
+    page6.style.display = "none"
+    page7.style.display = "none"
+}
+
+function showPage4() {
+    page1.style.display = "none"
+    page2.style.display = "none"
+    page3.style.display = "none"
+    page4.style.display = "block"
+    page5.style.display = "none"
+    page6.style.display = "none"
+    page7.style.display = "none"
+}
+
+
+function showPage5() {
+    page1.style.display = "none"
+    page2.style.display = "none"
+    page3.style.display = "none"
+    page4.style.display = "none"
+    page5.style.display = "block"
+    page6.style.display = "none"
+    page7.style.display = "none"
+}
+
+function showPage6() {
+    page1.style.display = "none"
+    page2.style.display = "none"
+    page3.style.display = "none"
+    page4.style.display = "none"
+    page5.style.display = "none"
+    page6.style.display = "block"
+    page7.style.display = "none"
+}
+
+function showPage7() {
+    page1.style.display = "none"
+    page2.style.display = "none"
+    page3.style.display = "none"
+    page4.style.display = "none"
+    page5.style.display = "none"
+    page6.style.display = "none"
+    page7.style.display = "block"
+}
+
+showPage1()
+
